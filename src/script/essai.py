93b02eb0ac646cfbe67a61b295ea8e6123405d79
Raclax <clarasachot@gmail.com>
2024-05-12 17:17:59 +0200

    docs : ajout des docstrings sur la fonction de création du dataset

--- src/script/essai.py
+++ src/script/essai.py
@@ -1,9 +1,41 @@
+"""CRéateur de dataset
+
+Ce script permet de récupérer toutes les pages articles à partir d'une 
+page principale d'amazon pour ensuite en extraire les reviews : le texte, l'id
+de la personne et la note associée.
+
+Ce script contient également one fonction pour transformer ces informations en un fichier CSV.
+
+A noter que bien qu'il devrait fonctionner en théorie, en pratique ce n'est pas le cas :
+Amazon fait en sorte que les données scrappées arrivent avec des soucis d'encodage et je 
+n'ai pas tu y trouver de solution.
+
+"""
+
+
 import requests
 from bs4 import BeautifulSoup as soup
 from time import sleep
+import csv
 
 def find_pages(url_base, headers):
     
+    """
+    Parcourt les pages de liste de livres sur Amazon pour collecter les liens vers les pages individuelles des livres.
+
+    Parameters
+    ----------
+    - url_base (str) : L'URL de base du site Amazon.
+    - headers (dict) : le headers
+
+    Returns
+    -------
+    - list : Une liste de chaînes contenant les URL des pages de détail des livres.
+
+    En plus :
+    - Pause de 2 secondes entre chaque requête pour éviter le blocage par le serveur d'Amazon.
+    """
+     
     page = 0
     lien = []
     while True:
@@ -21,10 +53,25 @@ def find_pages(url_base, headers):
         if not articles:
             break
         page += 1
-        sleep(2)
+        sleep(2) #2 secondes entre chque requêtes
     return lien
 
 def find_comms (lst, headers):
+
+    """
+    Extrait les commentaires, les notes et les identifiants des utilisateurs à partir des pages de livres sur Amazon.
+    
+    Parameters
+    ----------
+    - lst (list) : Liste des URL des pages de livres à analyser.
+    - headers (dict) : Les en-têtes HTTP utilisés pour les requêtes.
+
+    Returns
+    -------
+    - list : Une liste de dictionnaires où chaque dictionnaire contient l'identifiant de l'utilisateur, le commentaire, 
+             la note numérique et la note sous forme de texte.
+    """
+
     tout=[]
     for url in lst :
         html = requests.get(url, headers=headers)
@@ -32,11 +79,11 @@ def find_comms (lst, headers):
         comms = soup(html.text, 'html.parser')
         #print(comms)
         comments = comms.find_all('span', {'class': 'a-size-base review-text review-text-content'})
-    ratings = comms.find_all('span', {'class': 'a-icon-alt'})
-    id = comms.find_all('span', {'class': 'a-profile-name'})
+        ratings = comms.find_all('span', {'class': 'a-icon-alt'})
+        id = comms.find_all('span', {'class': 'a-profile-name'})
 
-    for i in range (0, len(comments)):
-        tout.append({"Identifiant" : id[i],
+        for i in range (0, len(comments)):
+            tout.append({"Identifiant" : id[i],
                     "Commentaire" : comments[i],
                     "Note" : int(ratings[i]),
                     "Note_str" : ratings[i]
@@ -45,6 +92,20 @@ def find_comms (lst, headers):
 
 
 def to_csv (tout) :
+    
+    """
+    Sauvegarde les données extraites dans un fichier CSV.
+
+    Parameters
+    ----------
+    - tout (list) : Liste des données à écrire dans le fichier CSV. Chaque élément de la liste est un dictionnaire
+                    représentant un commentaire et ses métadonnées associées.
+
+    Returns
+    -------
+    - Crée un fichier 'dataset.csv' dans le répertoire de travail actuel et écrit les données dedans.
+    """
+    
     titres = ['id', 'text', 'label', 'label_text']
 
     with open('dataset.csv', 'w', newline='') as file:
@@ -54,7 +115,17 @@ def to_csv (tout) :
         for row in tout:
             writer.writerow(row)
 
-def main(recherche, num_pages):
+def main():
+
+    """
+    Fonction principale.
+
+    Parameters
+    ----------
+    - recherche (str) : Terme de recherche à utiliser pour filtrer les livres sur Amazon.
+    - num_pages (int) : Nombre de pages à parcourir pour la collecte de liens.
+    """
+
     url_base = "https://www.amazon.fr"
     results = []
     headers = {
@@ -73,7 +144,6 @@ def main(recherche, num_pages):
         
     
     to_csv (results)
-        # Pause de 2 secondes après chaque page
     
 
 main()
